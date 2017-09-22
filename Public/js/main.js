1689c21f38bdcd4fe5db092cce617068b574d697
ngxz <ngxz92@163.com>
2017-09-22 10:03:00 +0800

    9.22

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,3 +1,11 @@
+//百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e47e4f1811685155b314133ec7606da3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 $(function(){
 	//首页图片区鼠标移入效果
 	$(".photo ul li").hover(function(){
